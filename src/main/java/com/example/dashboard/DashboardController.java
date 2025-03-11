1074a77e4e5c4eb30bb7a695423d43b2ed16d4fe
devgemmy <anthonia@nugitech.com>
2025-03-11 17:26:02 +0000

    piechart and barchart updated and ready

--- src/main/java/com/example/dashboard/DashboardController.java
+++ src/main/java/com/example/dashboard/DashboardController.java
@@ -6,24 +6,37 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Side;
+import javafx.scene.chart.BarChart;
 import javafx.scene.chart.PieChart;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 
 import java.net.URL;
+import java.sql.*;
+import java.util.HashMap;
 import java.util.ResourceBundle;
 
 public class DashboardController implements Initializable {
     @FXML private PieChart totalCostsChart;
     @FXML private Button addInvoiceButton;
     @FXML private Label costLabel;
+    @FXML private TextField searchField;
+    @FXML private BarChart<String, Integer> averageUniCostsBarChart;
+    @FXML TableView<Invoice> invoiceTable;
+    @FXML private TableColumn<Invoice, String> invID;
+    @FXML private TableColumn<Invoice, String> invName;
+    @FXML private TableColumn<Invoice, String> invUniDts;
+    @FXML private TableColumn<Invoice, String> invCourDts;
+    @FXML private TableColumn<Invoice, Integer> invCourFees;
+    @FXML private TableColumn<Invoice, String> invFoods;
+    @FXML private TableColumn<Invoice, String> invSports;
+    @FXML private TableColumn<Invoice, String> invDate;
 
-    @FXML
-    private void loadTotalCostsChart() {
+    // Load total costs across all Universities
+    @FXML private void loadTotalCostsChart() {
 
         ObservableList<PieChart.Data> pieData =
                 FXCollections.observableArrayList(
@@ -35,11 +48,11 @@ public class DashboardController implements Initializable {
         totalCostsChart.setData(pieData);
         totalCostsChart.setTitle("Total Costs in 2022 for Business");
         totalCostsChart.setClockwise(false);
-        // totalCostsChart.setLegendSide(Side.TOP);
+        totalCostsChart.setLegendSide(Side.BOTTOM);
 
         costLabel.setTextFill(Color.BLACK);
         costLabel.setStyle("-fx-font: 20 Arial;");
-        // costLabel.setText("£" + String.valueOf(totalCostsChart.getData().getFirst().getPieValue()));
+        costLabel.setText("£" + String.valueOf(totalCostsChart.getData().getFirst().getPieValue()));
 
         for (final PieChart.Data data : totalCostsChart.getData()) {
             data.getNode().addEventHandler(
@@ -56,6 +69,43 @@ public class DashboardController implements Initializable {
         }
     }
 
+    // Load Average costs for each category in a selected University in a Bar Chart
+    @FXML private void loadAverageCostsChart() {
+        // Create Series instance with data types
+        XYChart.Series<String, Integer> courseCosts = new XYChart.Series<>();
+        courseCosts.setName("Courses"); // This is one of the Legends
+        // XYChart.Data(xAxis, yAxis);
+        courseCosts.getData().add(new XYChart.Data<>("2020", 25601));
+        courseCosts.getData().add(new XYChart.Data<>("2021", 20148));
+        courseCosts.getData().add(new XYChart.Data<>("2022", 12500));
+        courseCosts.getData().add(new XYChart.Data<>("2023", 122450));
+        courseCosts.getData().add(new XYChart.Data<>("2024", 54550));
+
+        XYChart.Series<String, Integer> foodCosts = new XYChart.Series<>();
+        foodCosts.setName("Food");
+        foodCosts.getData().add(new XYChart.Data<>("2020", 9560));
+        foodCosts.getData().add(new XYChart.Data<>("2021", 8148));
+        foodCosts.getData().add(new XYChart.Data<>("2022", 4200));
+        foodCosts.getData().add(new XYChart.Data<>("2023", 7480));
+        foodCosts.getData().add(new XYChart.Data<>("2024", 2950));
+
+        XYChart.Series<String, Integer> sportsCosts = new XYChart.Series<>();
+        sportsCosts.setName("Sports");
+        sportsCosts.getData().add(new XYChart.Data<>("2020", 4560));
+        sportsCosts.getData().add(new XYChart.Data<>("2021", 8148));
+        sportsCosts.getData().add(new XYChart.Data<>("2022", 1200));
+        sportsCosts.getData().add(new XYChart.Data<>("2023", 3450));
+        sportsCosts.getData().add(new XYChart.Data<>("2024", 9250));
+
+        // Add the series in the Bar Chart;
+        averageUniCostsBarChart.getData().add(courseCosts);
+        averageUniCostsBarChart.getData().add(foodCosts);
+        averageUniCostsBarChart.getData().add(sportsCosts);
+        averageUniCostsBarChart.setTitle("Average Costs Per University in March");
+        averageUniCostsBarChart.setLegendSide(Side.TOP);
+    }
+
+    // Alert the user before opening new Generate Invoice screen
     private void generateInvoice() {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle("Invoice");
@@ -64,11 +114,37 @@ public class DashboardController implements Initializable {
         alert.showAndWait();
     }
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        loadTotalCostsChart();
+    // Setup Connection to the SQLite Database
+    private Connection connectToDatabase() {
+        // JDBC stands for Java Database Connector
+        String driver = "jdbc:sqlite", db = "/Users/macbookair/Documents/BRUNEL/YEAR 1/Group Project B/UMS-DB.db";
+        final String DB_URL = driver + ":" + db;
+        Connection conn = null;
 
-        // Set event listeners
+        try {
+            conn = DriverManager.getConnection(DB_URL);
+            System.out.println("Connected to SQL DB successfully.");
+        } catch (Exception e) {
+            System.out.println("ERROR: " + e);
+            System.out.println("Database failed to connect.");
+        }
+
+        return conn;
+    }
+
+    // Template function for each SQL query
+    public ResultSet queryTheDB(String query) throws SQLException {
+        Connection conn = connectToDatabase();
+        try (PreparedStatement sqlStatement = conn.prepareStatement(query)) {
+            return sqlStatement.executeQuery();
+        }
+    }
+
+    // Initialise Data and required functions.
+    @Override public void initialize(URL url, ResourceBundle resourceBundle) {
+        loadTotalCostsChart();
+        loadAverageCostsChart();
+
         addInvoiceButton.setOnAction(event -> generateInvoice());
     }
 }
