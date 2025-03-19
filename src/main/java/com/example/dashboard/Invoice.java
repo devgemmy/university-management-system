ac93a6d49d9f78108ced302c671cbfbf3e2db977
devgemmy <anthonia@nugitech.com>
2025-03-19 09:09:41 +0000

    UI design changes

--- src/main/java/com/example/dashboard/Invoice.java
+++ src/main/java/com/example/dashboard/Invoice.java
@@ -15,6 +15,7 @@ public class Invoice {
     private double totalFoodCost;
     private String invoiceDate;
 
+    // Constructor overloading for default and parameterized constructors
     public Invoice() {
         this.institutionDetails = new HashMap<>();
         this.courseList = new HashMap<>();
@@ -23,9 +24,9 @@ public class Invoice {
     }
 
     public Invoice(String invoiceId, String studentName, String invoiceDate, double courseFees,
-                  double sportsCost, double foodCost, Map<String, String> courseList,
-                  Map<String, String> institutionDetails, Map<String, Double> sportsActivities,
-                  Map<String, Double> foodItems) {
+            double sportsCost, double foodCost, Map<String, String> courseList,
+            Map<String, String> institutionDetails, Map<String, Double> sportsActivities,
+            Map<String, Double> foodItems) {
         this.invoiceID = invoiceId;
         this.studentName = studentName;
         this.invoiceDate = invoiceDate;
@@ -39,41 +40,120 @@ public class Invoice {
     }
 
     // Getters and setters
-    public String getInvoiceID() { return invoiceID; }
-    public void setInvoiceID(String invoiceID) { this.invoiceID = invoiceID; }
+    public String getInvoiceID() {
+        return invoiceID;
+    }
 
-    public String getStudentName() { return studentName; }
-    public void setStudentName(String studentName) { this.studentName = studentName; }
+    public void setInvoiceID(String invoiceID) {
+        this.invoiceID = invoiceID;
+    }
 
-    public Map<String, String> getInstitutionDetails() { return institutionDetails; }
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public Map<String, String> getInstitutionDetails() {
+        return institutionDetails;
+    }
+
     public void setInstitutionDetails(Map<String, String> institutionDetails) {
         this.institutionDetails = new HashMap<>(institutionDetails);
     }
 
-    public Map<String, String> getCourseList() { return courseList; }
+    public Map<String, String> getCourseList() {
+        return courseList;
+    }
+
     public void setCourseList(Map<String, String> courseList) {
         this.courseList = new HashMap<>(courseList);
     }
 
-    public double getCourseInvFees() { return courseInvFees; }
-    public void setCourseInvFees(double courseInvFees) { this.courseInvFees = courseInvFees; }
+    public double getCourseInvFees() {
+        return courseInvFees;
+    }
 
-    public Map<String, Double> getSportsActivities() { return sportsActivities; }
+    public void setCourseInvFees(double courseInvFees) {
+        this.courseInvFees = courseInvFees;
+    }
+
+    public Map<String, Double> getSportsActivities() {
+        return sportsActivities;
+    }
+
     public void setSportsActivities(Map<String, Double> sportsActivities) {
         this.sportsActivities = new HashMap<>(sportsActivities);
         this.totalSportsCost = sportsActivities.values().stream().mapToDouble(Double::doubleValue).sum();
     }
 
-    public double getTotalSportsCost() { return totalSportsCost; }
+    public double getTotalSportsCost() {
+        return totalSportsCost;
+    }
 
-    public Map<String, Double> getFoodItems() { return foodItems; }
+    public Map<String, Double> getFoodItems() {
+        return foodItems;
+    }
+
     public void setFoodItems(Map<String, Double> foodItems) {
         this.foodItems = new HashMap<>(foodItems);
         this.totalFoodCost = foodItems.values().stream().mapToDouble(Double::doubleValue).sum();
     }
 
-    public double getTotalFoodCost() { return totalFoodCost; }
+    public double getTotalFoodCost() {
+        return totalFoodCost;
+    }
 
-    public String getInvoiceDate() { return invoiceDate; }
-    public void setInvoiceDate(String invoiceDate) { this.invoiceDate = invoiceDate; }
+    public String getInvoiceDate() {
+        return invoiceDate;
+    }
+
+    public void setInvoiceDate(String invoiceDate) {
+        this.invoiceDate = invoiceDate;
+    }
 }
+
+/*
+ * 
+ * -invoicelD: String
+ * -studentName: String
+ * -institutionDetails: Map<String, String>
+ * -courseList: Map<String, String>
+ * -courseInvFees: double
+ * -sportsActivities: Map<String, Double>
+ * -totalSportsCost: double
+ * -foodItems: Map<String, Double>
+ * -totalFoodCost: double
+ * -invoiceDate: String
+ * 
+ * ----------------------------------------------
+ * 
+ * +public Invoice(String invoiceId, String studentName, String invoiceDate,
+ * double courseFees, double sportsCost, double foodCost, Map<String, String>
+ * courseList, Map<String, String> institutionDetails, Map<String, Double>
+ * sportsActivities, Map<String, Double> foodItems)
+ * 
+ * +getInvoiceID(): String
+ * +setInvoiceID(): void
+ * +getStudentName(): String
+ * +setStudentName(): void
+ * +getInstitutionDetails(): Map<String, String>
+ * +setInstitutionDetails(): void
+ * +getCourseList(): Map<String, String>
+ * +setCourseList(): void
+ * +getCourseInvFees(): double
+ * +setCourseInvFees(): void
+ * +getSportsActivities(): Map<String, Double>
+ * +setSportsActivities(): void
+ * +getTotalSportsCost(): double
+ * +getFoodItems(): Map<String, Double>
+ * +setFoodItems(): void
+ * +getTotalFoodCost(): double
+ * +getInvoiceDate(): String
+ * +setInvoiceDate(): void
+ * 
+ * 
+ * 
+ */
